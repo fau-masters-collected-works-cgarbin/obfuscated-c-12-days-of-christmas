0355f174b79e1d02eff122bb7ea826fc3ed0b976
Christian Garbin <garbin.christian@gmail.com>
2020-11-08 11:19:27 -0500

    Improve comments

--- mystery09.c
+++ mystery09.c
@@ -6,6 +6,8 @@
 // Instead of using magic numbers for "t", change to constants to make it
 // easier to follow what branch of the code is being executed in the next call
 // and what that branch of code does
+// Also use better names for the function parameters (still cryptic, but at
+// least now their order is apparent in their names)
 
 char *large_string =
     "@n'+,#'/*{}w+/w#cdnr/+,{}r/*de}+,/*{*+,/w{%+,/w#q#n+,/#{l+,/n{n+,/+#n+,/"
@@ -22,52 +24,55 @@ char *small_string =
 #define SWAP_VARIABLES (-73)
 #define ADVANCE_OR_PRINT (-51)
 
-f(t, _, a) char *a;
+f(p1, p2, p3) char *p3;
 {
-    if (t == GOTO_TO_SLASH) {
-        if (*a == '/') {
+    // Called recursively until a / is found in p3
+    if (p1 == GOTO_TO_SLASH) {
+        if (*p3 == '/') {
             return 1;
         }
-        int tmp = f(ADVANCE_OR_PRINT, *a, small_string);
-        return f(GOTO_TO_SLASH, tmp, a + 1);
+        int tmp = f(ADVANCE_OR_PRINT, *p3, small_string);
+        return f(GOTO_TO_SLASH, tmp, p3 + 1);
     }
 
-    if (t == 1) {
+    if (p1 == 1) {
         return f(2, 2, "%s");
     }
 
-    if (t == 2) {
-        int tmp1 = f(SWAP_VARIABLES, 0, a + 1);
-        int tmp2 = f(SWAP_VARIABLES, 1 - _, tmp1 + a);
-        f(SWAP_VARIABLES, -13, a + tmp2);
+    if (p1 == 2) {
+        int tmp1 = f(SWAP_VARIABLES, 0, p3 + 1);
+        int tmp2 = f(SWAP_VARIABLES, 1 - p2, tmp1 + p3);
+        f(SWAP_VARIABLES, -13, p3 + tmp2);
     }
 
-    if (t < (SWAP_VARIABLES+1)) {
-        return f(_, t, large_string);
+    // Swap p1 and p2
+    if (p1 < (SWAP_VARIABLES+1)) {
+        return f(p2, p1, large_string);
     }
 
-    if (t < (ADVANCE_OR_PRINT+1)) {
-        if (_ == *a) {
-            return putchar(a[31]);
+    // Find p2 in p3 and print it
+    if (p1 < (ADVANCE_OR_PRINT+1)) {
+        if (p2 == *p3) {
+            return putchar(p3[31]);
         }
-        return f(ADVANCE_OR_PRINT, _, a + 1);
+        return f(ADVANCE_OR_PRINT, p2, p3 + 1);
     }
 
-    if (t > 1) {
-        if (t < _) {
-            f(t + 1, _, a);
+    if (p1 > 1) {
+        if (p1 < p2) {
+            f(p1 + 1, p2, p3);
         }
-        if (f(SWAP_VARIABLES, -27 + t, a) && t == 2) {
-            if (_ < 13) {
-                return f(2, _ + 1, "%s %d %d\n");
+        if (f(SWAP_VARIABLES, -27 + p1, p3) && p1 == 2) {
+            if (p2 < 13) {
+                return f(2, p2 + 1, "%s %d %d\n");
             }
             return 9;
         }
         return 16;
     }
 
-    if (t < 0) {
-        return f((*a == '/') + t, _, a + 1);
+    if (p1 < 0) {
+        return f((*p3 == '/') + p1, p2, p3 + 1);
     }
 }
 
