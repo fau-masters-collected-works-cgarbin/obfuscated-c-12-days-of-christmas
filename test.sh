7c8e49c0fd546d88c34c05492422fea3056c4d14
Christian Garbin <garbin.christian@gmail.com>
2020-11-11 07:53:49 -0500

    Improve test script

--- test.sh
+++ test.sh
@@ -4,18 +4,17 @@ rm "*.output" >/dev/null 2>&1
 
 for file in *.c
 do
+    rm "a.out" >/dev/null 2>&1 # to catch compilation errors
+    gcc "$file" >/dev/null 2>&1
+
     if [ "$file" == "mystery01.c" ]; then
-        rm "a.out" >/dev/null 2>&1 # to catch compilation errors
-        echo "original version"
-        gcc "$file" >/dev/null 2>&1
+        echo "saving original text"
         ./a.out > original
     else
-        rm "a.out" >/dev/null 2>&1 # to catch compilation errors
         echo "testing $file"
-        gcc "$file" >/dev/null 2>&1
         ./a.out > "$file.output"
         if ! diff original "$file.output"; then
-            echo "File does not match the original text"
+            echo "$file does not match the original text"
             exit 1
         fi
     fi
