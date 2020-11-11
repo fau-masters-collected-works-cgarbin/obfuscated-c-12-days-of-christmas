b94cd341b746e73a907b28baee7680673f978b52
Christian Garbin <garbin.christian@gmail.com>
2020-11-10 20:51:37 -0500

    Improve comments and variable names

--- mystery17.c
+++ mystery17.c
@@ -1,33 +1,25 @@
 #include <stdio.h>
 
-// Step 16 - split out code that prints a piece of text
-// To help see the logic of the main function, the code that prints one piece
-// of text is moved to its own function
-// It is also clear now that the small string is acting as a cypher: a
-// character from the large string is translated into a character 31
-// positions ahead in the small string
-// The call to decode the text when p1 == 2 is also replaced with a direct
-// call to the decode function - this makes it clearer that p1 == 2 is the
-// initialization code for the program
-// Finally, if we move the "p1 < 0" code up, we can remove the "p1 > 1" test
-// At this point, with the code pretty much reworked, it was also changed
-// to modern C, e.g. declare function types, etc.
+// Step 17 - the final version
+// This version splits out of the main loop the functions that decode and
+// and print a specific piece of the text.
+// The main function now has only the logic.
 
-char *large_string =
+char *encoded_text =
     "@n'+,#'/*{}w+/w#cdnr/+,{}r/*de}+,/*{*+,/w{%+,/w#q#n+,/#{l+,/n{n+,/+#n+"
     ",/#\;#q#n+,/+k#;*+,/'r :'d*'3,}{w+K w'K:'+}e#';dq#'l \q#'+d'K#!/+k#;q#"
     "'r}eKK#}w'r}eKK{nl]'/#;#q#n'){)#}w'){){nl]'/+#n';d}rw' i;# \){nl]!/n{n"
     "#'; r{#w'r nc{nl]'/#{l,+'K {rw' iK{;[{nl]'/w#q#n'wk nw' \iwk{KK{nl]!/w"
     "{%'l##w#' i; :{nl]'/*{q#'ld;r'}{nlwb!/*de}'c \;;{nl'-{}rw]'/+,}##'*}#n"
     "c,',#nw]'/+kd'+e}+;#'rdq#w! nr'/ ') }+}{rl#'{n' ')# \}'+}##(!!/";
-char *small_string =
-    "!ek;dc i@bK'(q)-[w]*%n+r3#l,{}:\nuwloca-O;m .vpbks,fxntdCeghiry";
 
-#define PRINT_ONE_PIECE (0)
+char *cypher =
+    "!ek;dc i@bK'(q)-[w]*%n+r3#l,{}:"
+   "\nuwloca-O;m .vpbks,fxntdCeghiry";
 
 void decode_text(char *text) {
     while (*text != '/') {
-        char *p = small_string;
+        char *p = cypher;
         while (*text != *p) p++;
         putchar(p[31]);
         text++;
@@ -36,18 +28,17 @@ void decode_text(char *text) {
 
 void move_to_segment_and_decode(int segment) {
     int slashes_so_far = 0;
-    char *p = large_string;
+    char *p = encoded_text;
     while (slashes_so_far < segment) {
-        if (*p == '/') {
+        if (*p++ == '/') {
             slashes_so_far++;
         }
-        p++;
     }
     decode_text(p);
 }
 
 void print_day_preamble(int day) {
-    decode_text(large_string);
+    decode_text(encoded_text);
     move_to_segment_and_decode(day);
     move_to_segment_and_decode(13);
 }
@@ -66,7 +57,7 @@ int f(int p1, int p2, char *p3) {
         f(p1 + 1, p2, p3);
     }
 
-    if (f(-27 + p1, 0, large_string) && p1 == 2) {
+    if (f(-27 + p1, 0, encoded_text) && p1 == 2) {
         if (p2 < 13) {
             return f(2, p2 + 1, 0);
         }
