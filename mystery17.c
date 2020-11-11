f577853705bf3a555f508b46195a9bc46fdb8856
Christian Garbin <garbin.christian@gmail.com>
2020-11-11 07:32:35 -0500

    Restore version 17

--- mystery17.c
+++ mystery17.c
@@ -15,7 +15,7 @@ char *encoded_text =
 
 char *cypher =
     "!ek;dc i@bK'(q)-[w]*%n+r3#l,{}:"
-   "\nuwloca-O;m .vpbks,fxntdCeghiry";
+    "\nuwloca-O;m .vpbks,fxntdCeghiry";
 
 void decode_text(char *text) {
     while (*text != '/') {
@@ -43,11 +43,23 @@ void print_day_preamble(int day) {
     move_to_segment_and_decode(13);
 }
 
-int main(int argc, char *argv[]) {
-    for (int day = 1; day < 13; day++) {
-        print_day_preamble(day);
-        for (int i = day + 1; i >= 2; i--) {
-            move_to_segment_and_decode(27 - i);
-        }
+int f(int p1, int p2) {
+    if (p1 == 2) {
+        print_day_preamble(p2 - 1);
     }
+
+    if (p1 < p2) {
+        f(p1 + 1, p2);
+    }
+
+    move_to_segment_and_decode(27 - p1);
+
+    if (p1 == 2 && p2 < 13) {
+        f(2, p2 + 1);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    // Call with the value that kicks off the code (2)
+    f(2, 2);
 }
